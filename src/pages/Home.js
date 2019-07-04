8d19fbee145eb8d10c08b2ff2d1586666101271d
Louis Luo <louis.luo@alliance.tw>
2019-07-04 12:08:20 +0800

    新增Hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,16 @@
 import { h } from 'hyperapp'
 
 export const Home = () => {
-  return (
-    <div>
-      <section class='hero'>
-        <div class='hero-body'>
-          <div class='container'>
-            <h1 class='title'>
-              <h1>Home</h1>
-            </h1>
-            <h2 class='subtitle'>Hero subtitle</h2>
-          </div>
+    return (
+        <div>
+            <section class='hero is-medium is-dark is-bold'>
+                <div class='hero-body'>
+                    <div class='container'>
+                        <h1 class='title'>HyperApp 練習用購物網站</h1>
+                        <h2 class='subtitle'>電影購物車</h2>
+                    </div>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  )
+    )
 }
