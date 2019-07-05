6cccfeccd8a6ce3c1376ef0bad1fb857abec7423
Louis Luo <louis.luo@alliance.tw>
2019-07-05 17:07:30 +0800

    完成分頁

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import App from './App'
 
 import * as Home from './pages/Home'
 import * as Cart from './components/Cart'
+import * as Product from './pages/Product'
 
 const state = {
     location: location.state,
     home: Home.state,
+    product: Product.state,
     cart: Cart.state
 }
 const actions = {
     location: location.actions,
     home: Home.actions,
+    product: Product.actions,
     cart: Cart.actions
 }
 
--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ export const Routes = ({ state, actions }) => {
     return (
         <Switch>
             <Route path='/' render={Home({ state: state.home, actions: actions.home })} />
-            <Route path='/product/:id' render={Product} />
+            <Route path='/product/:id' render={Product({ state: state.product, actions: actions.product })} />
             <Route path='/about' render={About} />
             <Route render={NoMatch} />
         </Switch>
